4e9ee872b99c465627804e821d8d1b6743ba6fb5
송재욱 <haensol976520@gmail.com>
2025-01-05 14:23:47 +0900

    stype

--- client/src/shared/Color.ts
+++ client/src/shared/Color.ts
@@ -18,4 +18,15 @@ export const Colors = {
   accent: {
     blue: "#528bff",
   },
+
+  syntax: {
+    keyword: "#c678dd",
+    string: "#98c379",
+    number: "#d19a66",
+    comment: "#5c6370",
+    operator: "#56b6c2",
+    function: "#61afef",
+    variable: "#e06c75",
+    type: "#e5c07b",
+  },
 } as const;
--- client/src/widgets/Codespace.tsx
+++ client/src/widgets/Codespace.tsx
@@ -1,13 +1,18 @@
 import { useState, useRef, KeyboardEvent } from "react";
 import { Colors } from "../shared/color";
 import styled from "styled-components";
+import { highlight, languages } from "prismjs";
+import "prismjs/components/prism-typescript";
+import "prismjs/components/prism-javascript";
 
 const CodespaceContainer = styled.div`
   width: 95%;
-  padding: 20px;
+  min-height: 300px;
   position: relative;
   background-color: ${Colors.background.primary};
   border-radius: 4px;
+  padding: 0;
+  margin: 20px auto;
 `;
 
 const LineNumbers = styled.div`
@@ -16,8 +21,7 @@ const LineNumbers = styled.div`
   top: 0px;
   bottom: 0px;
   width: 40px;
-  padding-right: 10px;
-  padding-top: 32px;
+  padding: 12px 10px;
   background-color: ${Colors.background.secondary};
   border-right: 1px solid ${Colors.border.primary};
   color: ${Colors.text.secondary};
@@ -26,22 +30,31 @@ const LineNumbers = styled.div`
   line-height: 1.5;
   text-align: right;
   user-select: none;
-  border-radius: 4px;
+  border-top-left-radius: 4px;
+  border-bottom-left-radius: 4px;
 `;
 
 const CodeInput = styled.textarea`
-  width: 90%;
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
   min-height: 300px;
-  padding: 12px 12px 12px 55px;
+  padding: 12px 20px 12px 55px;
   font-family: "Hack", "Consolas", monospace;
   font-size: 14px;
   line-height: 1.5;
   border: 0px;
   border-radius: 4px;
   resize: vertical;
-  background-color: ${Colors.background.primary};
-  color: ${Colors.text.primary};
+  background-color: transparent;
+  color: transparent;
+  caret-color: ${Colors.text.primary};
   tab-size: 2;
+  white-space: pre-wrap;
+  word-wrap: break-word;
+  box-sizing: border-box;
 
   &::placeholder {
     color: ${Colors.text.placeholder};
@@ -49,6 +62,52 @@ const CodeInput = styled.textarea`
 
   &:focus {
     outline: none;
+  }
+`;
+
+const CodeDisplay = styled.pre`
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  min-height: 300px;
+  padding: 12px 12px 12px 55px;
+  margin: 0;
+  font-family: "Hack", "Consolas", monospace;
+  font-size: 14px;
+  line-height: 1.5;
+  pointer-events: none;
+  overflow: hidden;
+  white-space: pre-wrap;
+  word-wrap: break-word;
+  background: transparent;
+  box-sizing: border-box;
+  color: ${Colors.text.primary};
+
+  & .token.keyword {
+    color: ${Colors.syntax.keyword};
+  }
+  & .token.string {
+    color: ${Colors.syntax.string};
+  }
+  & .token.number {
+    color: ${Colors.syntax.number};
+  }
+  & .token.comment {
+    color: ${Colors.syntax.comment};
+  }
+  & .token.operator {
+    color: ${Colors.syntax.operator};
+  }
+  & .token.function {
+    color: ${Colors.syntax.function};
+  }
+  & .token.variable {
+    color: ${Colors.syntax.variable};
+  }
+  & .token.type {
+    color: ${Colors.syntax.type};
   }
 `;
 
@@ -90,6 +149,10 @@ export default function Codespace({ value = "", onChange }: CodespaceProps) {
     return Array.from({ length: lines }, (_, i) => <div key={i + 1}>{i + 1}</div>);
   };
 
+  const highlightCode = (code: string) => {
+    return highlight(code, languages.typescript, "typescript");
+  };
+
   return (
     <CodespaceContainer>
       <LineNumbers>{renderLineNumbers()}</LineNumbers>
@@ -101,6 +164,7 @@ export default function Codespace({ value = "", onChange }: CodespaceProps) {
         placeholder="코드 입력"
         spellCheck={false}
       />
+      <CodeDisplay dangerouslySetInnerHTML={{ __html: highlightCode(code) }} />
     </CodespaceContainer>
   );
 }
